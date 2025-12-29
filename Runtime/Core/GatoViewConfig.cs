2c777c02cb02200969790892adf0d78727c426d9
thatgato <thatgatooo@gmail.com>
2025-12-29 15:13:46 +0100

    Editor Setup UI

---
+++ Runtime/Core/GatoViewConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GatoView
+{
+    [CreateAssetMenu(fileName = "GatoView Configuration", menuName = "Scriptable Objects/GatoView")]
+    public class GatoViewConfig : ScriptableObject
+    {
+    
+    }
+}
